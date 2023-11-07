2e70edbc35ca8ebe6d41012a9a7cf98d9a2749c1
mariogarciasaz <129775404+mariogarciasaz@users.noreply.github.com>
2023-11-07 17:14:42 +0100

    Update settings.py

--- core/settings.py
+++ core/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-ef%*=d446r*z654q(f2)x_(2a&k*r1d78j525tf=$xszm=*iit
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['inventorycontrol.onrender.com']
 
 
 # Application definition
